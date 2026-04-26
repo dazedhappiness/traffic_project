20231d0f463ffad993ddcb02ebbcddf756342ffc
A <124581040+dazedhappiness@users.noreply.github.com>
2026-04-26 16:34:16 +0530

    Update vehicles.py

--- traffic_sim/vehicles.py
+++ traffic_sim/vehicles.py
@@ -1,12 +1,15 @@
 class Vehicle:
-    def __init__(self, vid, source, destination, speed=1):
+    def __init__(self, vid, source, destination, path, speed=1):
         self.vid = vid
         self.source = source
         self.destination = destination
         self.speed = speed
         self.current_road = None
         self.position_on_road = 0 
-        self.path = []
+        self.path = path
+        
+        colors = {"J3": "red", "J4": "blue", "J5": "green"}
+        self.color = colors.get(destination, "black")
 
     def move(self):
         if self.current_road:
